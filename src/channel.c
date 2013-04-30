153d3123141965ee765306b44ee0647d93078131
Jiajun <shenjiajun90@gmail.com>
2013-04-29 21:10:52 -0500

    Modify on Channel to support mode

--- src/channel.c
+++ src/channel.c
@@ -1,40 +1,40 @@
 #include "channel.h"
 void circulate_in_channel(channel_info* chan, char* message){
     // TO DO take care of modes for @ sign
-    //channel_info *chan = find_channel_by_nick(channel_nick); 
-    user_info *usr = (user_info*)malloc(sizeof(user_info)); 
+    //channel_info *chan = find_channel_by_nick(channel_nick);
+    user_info *usr = (user_info*)malloc(sizeof(user_info));
     int i;
     for(i=0;i<list_size(&chan->ci_users);i++){
-	usr = (user_info *)list_get_at( &chan->ci_users, i);
+        usr = (user_info *)list_get_at( &chan->ci_users, i);
         send_rpl(usr->ui_socket, message);
     }
 }
 bool is_channel_empty(channel_info* channel){
     if(list_size(&channel->ci_users)==0)
-	return 1;
-    else 
-	return 0;
+        return 1;
+    else
+        return 0;
 }
 
 bool is_user_on_channel( channel_info* channel, user_info* sender){
-    user_info *usr = (user_info*)malloc(sizeof(user_info)); 
+    user_info *usr = (user_info*)malloc(sizeof(user_info));
     int i;
     for(i = 0; i < list_size( &channel->ci_users); i++){
-	usr = (user_info *)list_get_at( &channel->ci_users, i);
-	if(usr->ui_socket==sender->ui_socket){	
-	    return 1;
+        usr = (user_info *)list_get_at( &channel->ci_users, i);
+        if(usr->ui_socket==sender->ui_socket){
+            return 1;
         }
     }
     return 0;
 }
 char * all_users_channel(char* nick){
     channel_info *chan = find_channel_by_nick(nick);
-    user_info *usr = (user_info*)malloc(sizeof(user_info));  
+    user_info *usr = (user_info*)malloc(sizeof(user_info));
     char buffer[MAX_MSG_LEN];
     int i;
     int cx=0;
     for(i=0;i<list_size(&chan->ci_users);i++){
-	usr = (user_info *)list_get_at(& chan->ci_users, i);
+        usr = (user_info *)list_get_at(& chan->ci_users, i);
         cx = snprintf ( buffer + cx, MAX_MSG_LEN -cx, "%s ", usr->ui_nick);
     }
     buffer[strlen(buffer)-1]='\0';
@@ -42,17 +42,31 @@ char * all_users_channel(char* nick){
     return all_users;
 }
 
+bool is_channel_on_list(char* nick){
+    int find = 0;
+    int i;
+    for(i = 0; i <list_size(&channel_list);i++)
+    {
+        if(strcmp(nick, ((channel_info *)list_get_at(&channel_list, i))->ci_nick)==0)
+        {
+            find = 1;
+            break;
+        }
+    }
+    return find;
+}
+
 
 
 channel_info* find_channel_by_nick(char* nick){
-    channel_info *chan = (channel_info*)malloc(sizeof(channel_info)); 
+    channel_info *chan = (channel_info*)malloc(sizeof(channel_info));
     channel_info* p_chan = init_channel(nick);
     int i;
     for(i=0;i<list_size(&channel_list);i++){
-	chan = (channel_info *)list_get_at( &channel_list, i);
-	if(strcmp(chan->ci_nick,nick)==0){	
-	    p_chan=chan;
-	    break;
+        chan = (channel_info *)list_get_at( &channel_list, i);
+        if(strcmp(chan->ci_nick,nick)==0){
+            p_chan=chan;
+            break;
         }
     }
     return p_chan;
@@ -60,6 +74,7 @@ channel_info* find_channel_by_nick(char* nick){
 channel_info* init_channel(char* nick ){
     channel_info *chan = (channel_info*)malloc(sizeof(channel_info));
     chan->ci_nick = strdup(nick);
+    chan->topicSet = 0;
     list_init(&chan->ci_users);
     list_append(&channel_list,chan);
     return chan;
--- src/channel.h
+++ src/channel.h
@@ -12,16 +12,22 @@
 typedef struct {
     char *ci_nick;
     list_t ci_users;
-
+    list_t ci_voiceUsers;
+    list_t ci_operatorUsers;
+    int moderateMode;
+    int topicMode;
+    char * topic;
+    int topicSet;
 } channel_info;
-    // TO DO MODES
-    // TO DO TOPICS
+// TO DO MODES
+// TO DO TOPICS
 list_t channel_list; //list of channel_info
 
 void circulate_in_channel( channel_info* channel, char* message);
 bool is_channel_empty( channel_info* channel);
 bool is_user_on_channel( channel_info* channel, user_info* sender);
 char * all_users_channel( char* nick);
+bool is_channel_on_list(char* nick);
 channel_info* find_channel_by_nick( char* nick);
 channel_info* init_channel( char* nick);
 
